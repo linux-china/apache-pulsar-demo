000d37ee3803ea804a9c21b4c8adf437e935a7ac
linux_china <libing.chen@gmail.com>
2021-09-02 11:54:06 -0700

    Add reactive pulsar client

--- src/main/java/org/mvnsearch/demo/PortalController.java
+++ src/main/java/org/mvnsearch/demo/PortalController.java
@@ -1,7 +1,14 @@
 package org.mvnsearch.demo;
 
+import com.github.lhotari.reactive.pulsar.adapter.MessageSpec;
+import com.github.lhotari.reactive.pulsar.adapter.ReactiveMessageSender;
+import com.github.lhotari.reactive.pulsar.adapter.ReactivePulsarClient;
+import org.apache.pulsar.client.api.Schema;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * Portal controller
@@ -10,8 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class PortalController {
+    private ReactiveMessageSender<String> messageSender;
+
+    public PortalController(ReactivePulsarClient reactivePulsarClient) {
+        messageSender = reactivePulsarClient
+                .messageSender(Schema.STRING)
+                .topic("test-topic")
+                .maxInflight(100)
+                .build();
+    }
+
     @GetMapping("/")
     public String index() {
         return "Hello world!";
+    }
+
+    @PostMapping("/send")
+    public Mono<String> send(@RequestBody String body) {
+        return messageSender
+                .sendMessage(Mono.just(MessageSpec.of(body)))
+                .map(messageId -> "Message: " + messageId);
     }
 }
