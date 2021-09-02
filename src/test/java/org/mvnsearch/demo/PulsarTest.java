f348189203f0d52fee54efcbb26c3762e6333879
linux_china <libing.chen@gmail.com>
2021-09-02 11:44:27 -0700

    Polishing

--- src/test/java/org/mvnsearch/demo/PulsarTest.java
+++ src/test/java/org/mvnsearch/demo/PulsarTest.java
@@ -1,11 +1,13 @@
 package org.mvnsearch.demo;
 
+import com.github.lhotari.reactive.pulsar.adapter.MessageResult;
+import com.github.lhotari.reactive.pulsar.adapter.ReactiveMessageConsumer;
+import com.github.lhotari.reactive.pulsar.adapter.ReactivePulsarClient;
 import org.apache.pulsar.client.api.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import reactor.core.publisher.Flux;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.stream.IntStream;
@@ -20,10 +22,12 @@ public class PulsarTest {
     private static final String SERVICE_URL = "pulsar://localhost:6650";
     private static final String TOPIC_NAME = "test-topic";
     private PulsarClient client;
+    private ReactivePulsarClient reactivePulsarClient;
 
     @BeforeAll
     public void setUp() throws Exception {
         client = PulsarClient.builder().serviceUrl(SERVICE_URL).build();
+        reactivePulsarClient = ReactivePulsarClient.create(client);
     }
 
     @AfterAll
@@ -50,19 +54,13 @@ public class PulsarTest {
     @Test
     public void testSubscribe() throws Exception {
         CountDownLatch latch = new CountDownLatch(1);
-        Consumer<byte[]> consumer = client.newConsumer()
+        ReactiveMessageConsumer<String> messageConsumer = reactivePulsarClient
+                .messageConsumer(Schema.STRING)
                 .topic(TOPIC_NAME)
-                .subscriptionType(SubscriptionType.Shared)
-                .subscriptionName("Demo-1")
-                .subscribe();
-        Flux.<PulsarMessageWrapper<byte[]>>create(sink -> {
-            consumer.receiveAsync().thenAccept(message -> {
-                sink.next(new PulsarMessageWrapper<>(consumer, message));
-            });
-        }).flatMap(message -> {
-            System.out.println(new String(message.getData()));
-            return message.acknowledgeAsync();
-        }).subscribe();
+                .consumerConfigurer(consumerBuilder -> consumerBuilder.subscriptionName("sub-1"))
+                .build();
+        messageConsumer.consumeMessages(messageFlux -> messageFlux.map(message -> MessageResult.acknowledge(message.getMessageId(), message.getValue())))
+                .subscribe(System.out::println);
         latch.await();
     }
 }
