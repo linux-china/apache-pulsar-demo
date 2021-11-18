985fd375cf6c7f9ac3a29084afe720e3b23a793b
linux_china <libing.chen@gmail.com>
2021-11-18 10:48:45 -0800

    Add testTopicSender

--- src/main/java/org/mvnsearch/demo/PulsarConfiguration.java
+++ src/main/java/org/mvnsearch/demo/PulsarConfiguration.java
@@ -1,8 +1,10 @@
 package org.mvnsearch.demo;
 
+import com.github.lhotari.reactive.pulsar.adapter.ReactiveMessageSender;
 import com.github.lhotari.reactive.pulsar.adapter.ReactivePulsarClient;
 import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -26,5 +28,14 @@ public class PulsarConfiguration {
     @Bean
     public ReactivePulsarClient reactivePulsarClient(@Autowired PulsarClient pulsarClient) {
         return ReactivePulsarClient.create(pulsarClient);
+    }
+
+    @Bean
+    public ReactiveMessageSender<String> testTopicSender(@Autowired ReactivePulsarClient reactivePulsarClient) {
+        return reactivePulsarClient
+                .messageSender(Schema.STRING)
+                .topic("test-topic")
+                .maxInflight(100)
+                .build();
     }
 }
